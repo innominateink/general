9a0902f97b38a9fa972b2d458e7f08799feedc80
chaosflaw <jpagranz@gmail.com>
2019-11-04 17:55:17 +0000

    Fixed deepMerge to account for null values

--- js/deepMerge.js
+++ js/deepMerge.js
@@ -1,12 +1,14 @@
-function deepMerge(state = {}, mergeMe) {
+function deepMerge(state, mergeMe) {
   let mergedObject = {...state}
 
   for (let prop in mergeMe) {
     let value = mergeMe[prop]
 
-    if (typeof value === 'object') mergedObject[prop] = deepMerge(state[prop], value)
+    if (typeof value === 'object' && value !== null) mergedObject[prop] = deepMerge(state[prop], value)
     else mergedObject[prop] = value
   }
 
   return mergedObject
 }
+
+module.exports = deepMerge
